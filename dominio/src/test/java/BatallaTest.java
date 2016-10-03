0ca742da4bc557d3112dc0ce6a3cff1c3eb4dc2d
Jorge <jorgejocampo@gmail.com>
2016-10-03 17:30:24 -0300

    Add files via upload
    Se agrega test para batalla.

---
+++ dominio/src/test/java/BatallaTest.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class BatallaTest {
+
+    @Test
+    public void debeDefinirElGanadorEntreDosBatallones(){
+        Batallon batallon1 = new Batallon();
+        Batallon batallon2 = new Batallon();
+        Batalla batalla = new Batalla(batallon1, batallon2);
+        Assert.assertEquals(batallon1, batalla.definirGanador());
+    }
+
+    @Test
+    public void debeDefinirElPremioDeUnaBatalla(){
+        Batalla batalla = crearBatalla();
+        Assert.assertEquals(new ArrayList<>(),batalla.definirPremio());
+    }
+
+    private Batalla crearBatalla(){
+        Batallon batallon1 = new Batallon();
+        Batallon batallon2 = new Batallon();
+        return new Batalla(batallon1, batallon2);
+    }
+}
