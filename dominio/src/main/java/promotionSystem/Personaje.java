94d660d67b57d4d0a74b67e8e85a6933d586d8d7
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2016-10-03 19:19:14 -0300

    Creacion Clase Personaje y respectivos Test

---
+++ dominio/src/main/java/promotionSystem/Personaje.java
@@ -0,0 +1,55 @@
+package promotionSystem;
+
+public class Personaje {
+	private int salud;
+	private int energia;
+	private int ataque;
+	
+	public Personaje(){
+		energia=100;
+		salud=100;
+		ataque=10;
+	}
+
+	public void atacar(Personaje atacado) {
+		if(puedeAtacar()){
+			atacado.serAtacado(ataque);
+			energia-=ataque;
+		}
+		
+		
+	}
+
+	private void serAtacado(int ataque) {
+		salud-=ataque;
+		
+	}
+
+	private boolean puedeAtacar() {
+		return energia>=ataque;
+	}
+
+	public int getSalud() {
+		return salud;
+	}
+
+	public int getEnergia() {
+		return energia;
+	}
+
+	public boolean estaVivo() {
+		return salud>0;
+	}
+
+	public void serCurado() {
+		salud=100;
+		
+	}
+
+	public void serEnergizado() {
+		energia=100;
+		
+	}
+
+	
+}
