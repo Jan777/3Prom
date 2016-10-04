0d9f172bee413bf0349be7ac8539ce3878492c86
JorgeJOcampo <j_ocampo@hotmail.com.ar>
2016-10-04 12:28:22 -0300

    Creado un builder para la alianza.

---
+++ dominio/src/main/java/promotionSystem/builder/AlianzaBuilder.java
@@ -0,0 +1,18 @@
+package promotionSystem.builder;
+
+import promotionSystem.Alianza;
+import promotionSystem.Personaje;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlianzaBuilder {
+
+    public static Alianza crearAlianza(int cantidadDePersonajes){
+        List<Personaje> personajes = new ArrayList<>();
+        for(int i= 0; i < cantidadDePersonajes; i++){
+            personajes.add(new Personaje());
+        }
+        return new Alianza(personajes);
+    }
+}
