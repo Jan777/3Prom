b693d89d11cb59f43495183f210538bdd773d653
JorgeJOcampo <jorgejocampo@gmail.com>
2016-11-03 11:26:43 -0300

    Eliminado código en desuso

--- dominio/src/main/java/promotionSystem/Personaje.java
+++ dominio/src/main/java/promotionSystem/Personaje.java
@@ -18,7 +18,7 @@ public abstract class Personaje implements Comparable<Personaje>{
 	protected int magia;
 	protected int experiencia;
 	protected int nivel;
-    protected static ArbolDeHabilidades arbolDeHabilidades = new ArbolDeHabilidades();
+	protected static ArbolDeHabilidades arbolDeHabilidades = new ArbolDeHabilidades();
 	protected int puntosDeHabilidad;
 	protected Alianza alianza;
 	protected int velocidad;
@@ -30,10 +30,10 @@ public abstract class Personaje implements Comparable<Personaje>{
 	protected int energiaMaxima;
 	protected Map<String, Hechizo> hechizos;
 	private Item arma;
-    private Item botas;
-    private Item casco;
-    private Item chaleco;
-    private Item escudo;
+	private Item botas;
+	private Item casco;
+	private Item chaleco;
+	private Item escudo;
 	private Inventario inventario = new Inventario();
 
 	public final void atacar(Personaje atacado) {
@@ -54,18 +54,18 @@ public abstract class Personaje implements Comparable<Personaje>{
 		}
 	}
 
-    public final void usarMagiaSupport(Personaje atacado, String conjuro) {
-        if (puedeAtacarConMagia()) {
-            int puntosAUsar = calcularPuntosDeMagia();
-            hechizar(conjuro, atacado, puntosAUsar);
-            energia -= puntosAUsar;
-            despuesDeAtacar();
-        }
-    }
+	public final void usarMagiaSupport(Personaje atacado, String conjuro) {
+		if (puedeAtacarConMagia()) {
+			int puntosAUsar = calcularPuntosDeMagia();
+			hechizar(conjuro, atacado, puntosAUsar);
+			energia -= puntosAUsar;
+			despuesDeAtacar();
+		}
+	}
 
-    public final void usarMagiaDeAlteracion(String conjuro) {
-        usarMagiaSupport(this,conjuro);
-    }
+	public final void usarMagiaDeAlteracion(String conjuro) {
+		usarMagiaSupport(this,conjuro);
+	}
 
 	public abstract void despuesDeAtacar();
 
@@ -86,17 +86,17 @@ public abstract class Personaje implements Comparable<Personaje>{
 			velocidad = 0;
 		}
 	}
-	
+
 	public void serReducidoLaDefensa(int puntos) {
 		defensa -= puntos;
 		if (defensa < 0) {
 			defensa= 0;
 		}
 	}
-	
+
 	public void serAumentadoLaDefensa(int puntos) {
 		defensa += puntos;
-		
+
 	}
 
 	private boolean puedeAtacar() {
@@ -107,23 +107,23 @@ public abstract class Personaje implements Comparable<Personaje>{
 		return energia >= magia;
 	}
 
-    public int getAtaque() {
-        return ataque;
-    }
+	public int getAtaque() {
+		return ataque;
+	}
 
-    public int getDefensa() {
-        return defensa;
-    }
+	public int getDefensa() {
+		return defensa;
+	}
 
-    public int getMagia() {
-        return magia;
-    }
+	public int getMagia() {
+		return magia;
+	}
 
-    public int getVelocidad() {
-        return velocidad;
-    }
+	public int getVelocidad() {
+		return velocidad;
+	}
 
-    public int getSalud() {
+	public int getSalud() {
 		return salud;
 	}
 
@@ -141,10 +141,10 @@ public abstract class Personaje implements Comparable<Personaje>{
 
 	public void serCuradoConMagia(int efecto) {
 		if (saludMaxima - salud > efecto){
-			salud += efecto;			
+			salud += efecto;
 		}
 		else {
-			salud = saludMaxima;			
+			salud = saludMaxima;
 		}
 	}
 
@@ -203,19 +203,19 @@ public abstract class Personaje implements Comparable<Personaje>{
 		return experiencia;
 	}
 
-    public int getNivel() {
-        return nivel;
-    }
+	public int getNivel() {
+		return nivel;
+	}
 
 	public int getPuntosDeHabilidad() {
 		return puntosDeHabilidad;
 	}
 
-    public List<String> habilidades(){
-        return arbolDeHabilidades.listaDeHabilidades();
-    }
+	public List<String> habilidades(){
+		return arbolDeHabilidades.listaDeHabilidades();
+	}
 
-    public final void  subirExperiencia(int experiencia) {
+	public final void  subirExperiencia(int experiencia) {
 		this.experiencia+=experiencia;
 		int nivelesSubidos = 0;
 		while (this.experiencia > experienciaPorNivel()) {
@@ -227,37 +227,28 @@ public abstract class Personaje implements Comparable<Personaje>{
 	}
 
 	public void subirNivel() {
-        nivel++;
-        puntosDeHabilidad++;
+		nivel++;
+		puntosDeHabilidad++;
 	}
 
 	public void subirHabilidad(String habilidad){
-        Habilidad habilidadASubir = arbolDeHabilidades.buscarHabilidad(habilidad);
-        if(arbolDeHabilidades.subirHabilidad(habilidadASubir)){
-            activarHabilidad(habilidadASubir);
-        }
-    }
+		Habilidad habilidadASubir = arbolDeHabilidades.buscarHabilidad(habilidad);
+		if(arbolDeHabilidades.subirHabilidad(habilidadASubir)){
+			activarHabilidad(habilidadASubir);
+		}
+	}
 
-    public void activarHabilidad(Habilidad habilidad){
-        List<Integer> statsASubir = habilidad.statsASubir();
-        ataque += statsASubir.get(0);
-        magia += statsASubir.get(1);
-        defensa += statsASubir.get(2);
+	public void activarHabilidad(Habilidad habilidad){
+		List<Integer> statsASubir = habilidad.statsASubir();
+		ataque += statsASubir.get(0);
+		magia += statsASubir.get(1);
+		defensa += statsASubir.get(2);
 	}
 
 	private int experienciaPorNivel() {
 		return (int) Math.pow(nivel, 2);
 	}
 
-	/*
-	 * public Item entregarItem() { return items.remove(0); }
-	 *
-	 * public boolean puedeDarItem() { return items.size()!=0; }
-	 *
-	 * public void recibirItem(Item item) { items.add(item); }
-	 */
-	// FIXME cambiar esto cuando se defina el tema de como el personajeEquipado
-	// conoce la alianza.
 	public void setAlianza(Alianza alianza) {
 		this.alianza = alianza;
 	}
@@ -315,10 +306,10 @@ public abstract class Personaje implements Comparable<Personaje>{
 	}
 
 	public void aceptarDesafio(Personaje enemigo){
-        sort(this.alianza.personajes);
-        sort(enemigo.alianza.personajes);
-        Batalla batalla = new Batalla((this.invocarAliados()), enemigo.invocarAliados());
-    }
+		sort(this.alianza.personajes);
+		sort(enemigo.alianza.personajes);
+		Batalla batalla = new Batalla((this.invocarAliados()), enemigo.invocarAliados());
+	}
 
 	public abstract void subirStats(int nivel);
 
@@ -354,27 +345,27 @@ public abstract class Personaje implements Comparable<Personaje>{
 		return radioDeAcccion.incluye(this.posicion);
 	}
 
-    public Item getArma() {
-        return arma;
-    }
+	public Item getArma() {
+		return arma;
+	}
 
-    public Item getBotas() {
-        return botas;
-    }
+	public Item getBotas() {
+		return botas;
+	}
 
-    public Item getCasco() {
-        return casco;
-    }
+	public Item getCasco() {
+		return casco;
+	}
 
-    public Item getChaleco() {
-        return chaleco;
-    }
+	public Item getChaleco() {
+		return chaleco;
+	}
 
-    public Item getEscudo() {
-        return escudo;
-    }
+	public Item getEscudo() {
+		return escudo;
+	}
 
-    public void recibirItem(Item item) {
+	public void recibirItem(Item item) {
 		inventario.add(item);
 	}
 
@@ -387,49 +378,49 @@ public abstract class Personaje implements Comparable<Personaje>{
 	}
 
 	public List<Item> getItems(){
-        return inventario.getListaDeItems();
-    }
-
-	public void equiparItem(Item item) throws Exception{
-        Item itemEquipado = (Item)this.getClass().getSuperclass().getSuperclass().getDeclaredField(item.getTipoDeItem()).get(this);
-	    if(itemEquipado == null){
-            Item itemEnInventario = inventario.buscarItem(item);
-            equipar(itemEnInventario);
-        }else{
-            desequiparItem(itemEquipado);
-            equiparItem(item);
-        }
+		return inventario.getListaDeItems();
 	}
 
-    private void equipar(Item item) throws Exception {
-        inventario.remove(item);
+	public void equiparItem(Item item) throws Exception{
+		Item itemEquipado = (Item)this.getClass().getSuperclass().getSuperclass().getDeclaredField(item.getTipoDeItem()).get(this);
+		if(itemEquipado == null){
+			Item itemEnInventario = inventario.buscarItem(item);
+			equipar(itemEnInventario);
+		}else{
+			desequiparItem(itemEquipado);
+			equiparItem(item);
+		}
+	}
+
+	private void equipar(Item item) throws Exception {
+		inventario.remove(item);
 		this.getClass().getSuperclass().getSuperclass().getDeclaredField(item.getTipoDeItem()).set(this, item);
-        activarItem(item);
-    }
+		activarItem(item);
+	}
 
-    public void desequiparItem(Item item) throws Exception {
-        desactivarItem(item);
-        this.getClass().getSuperclass().getSuperclass().getDeclaredField(item.getTipoDeItem()).set(this, null);
-        inventario.add(item);
-    }
+	public void desequiparItem(Item item) throws Exception {
+		desactivarItem(item);
+		this.getClass().getSuperclass().getSuperclass().getDeclaredField(item.getTipoDeItem()).set(this, null);
+		inventario.add(item);
+	}
 
-    private void activarItem(Item item) {
-        ataque += item.getSumadorDeAtaque();
-        magia += item.getSumadorDeMagia();
-        defensa += item.getSumadorDeDefensa();
-        velocidad += item.getSumadorDeVelocidad();
-    }
+	private void activarItem(Item item) {
+		ataque += item.getSumadorDeAtaque();
+		magia += item.getSumadorDeMagia();
+		defensa += item.getSumadorDeDefensa();
+		velocidad += item.getSumadorDeVelocidad();
+	}
 
-    private void desactivarItem(Item item) {
-        ataque -= item.getSumadorDeAtaque();
-        magia -= item.getSumadorDeMagia();
-        defensa -= item.getSumadorDeDefensa();
-        velocidad -= item.getSumadorDeVelocidad();
-    }
+	private void desactivarItem(Item item) {
+		ataque -= item.getSumadorDeAtaque();
+		magia -= item.getSumadorDeMagia();
+		defensa -= item.getSumadorDeDefensa();
+		velocidad -= item.getSumadorDeVelocidad();
+	}
 
-    public void agregarAInventario(Item item){
-        inventario.add(item);
-    }
+	public void agregarAInventario(Item item){
+		inventario.add(item);
+	}
 
 	public Inventario getInventario() {
 		return inventario;
@@ -472,7 +463,7 @@ public abstract class Personaje implements Comparable<Personaje>{
 	public void agregarHechizo(String conjuro, Hechizo hechizo) {
 		this.hechizos.put(conjuro, hechizo);
 	}
-	
+
 	public int getCantidadDeHechizos() {
 		return this.hechizos.size();
 	}
@@ -494,16 +485,16 @@ public abstract class Personaje implements Comparable<Personaje>{
 		radioDeAcccion.setCentro(punto);
 	}
 
-    @Override
-    public int compareTo(Personaje personaje) {
-        if (velocidad < personaje.velocidad) {
-            return -1;
-        }
-        if (velocidad > personaje.velocidad) {
-            return 1;
-        }
-        return 0;
-    }
+	@Override
+	public int compareTo(Personaje personaje) {
+		if (velocidad < personaje.velocidad) {
+			return -1;
+		}
+		if (velocidad > personaje.velocidad) {
+			return 1;
+		}
+		return 0;
+	}
 }
-	
+
 
