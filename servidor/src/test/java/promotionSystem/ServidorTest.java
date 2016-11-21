fa83da1ee388af6afc7be467942ef899d613451f
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2016-11-21 12:25:11 -0300

    Agregado de JOptionPane para mensajes en el servidor

--- servidor/src/test/java/promotionSystem/ServidorTest.java
+++ servidor/src/test/java/promotionSystem/ServidorTest.java
@@ -1,12 +1,18 @@
 package promotionSystem;
 
+import javax.swing.JOptionPane;
+
 import org.junit.Test;
 
 public class ServidorTest {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args){
 
-		Servidor server = new Servidor();
+		try {
+			Servidor server = new Servidor();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error al Inicial el Servidor", "Error",JOptionPane.ERROR_MESSAGE);
+		}
 
 	}
 }
