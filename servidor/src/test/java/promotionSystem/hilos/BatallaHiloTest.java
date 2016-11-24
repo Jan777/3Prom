8e100caa118c603d17c34c405ad37d7f37c6aac6
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2016-11-24 17:49:24 -0300

    Agregado de test de batallaHilo

--- servidor/src/test/java/promotionSystem/hilos/BatallaHiloTest.java
+++ servidor/src/test/java/promotionSystem/hilos/BatallaHiloTest.java
@@ -37,6 +37,25 @@ public class BatallaHiloTest {
         assertEquals(20, alianza1.getPersonajes().get(1).getExperiencia());
         assertEquals(5, alianza1.getPersonajes().get(1).getNivel());
     }
+    
+    @Test
+    public void siLaAlianza2GanaSusIntegrantesDebenRecibirExperiencia() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        Method metodo = batallaReflection.getDeclaredMethod("aumentarExperienciaDeLosGanadores");
+        metodo.setAccessible(true);
+        set("cantidadMuertesAlianza1", 2);
+        set("alianza1", crearAlianza(2));
+        set("alianza2", crearAlianza(2));
+        set("subirExperienciaConMuertesDeAlianza1", 50);
+        metodo.invoke(batalla);
+        Field alianzaReflection = batallaReflection.getDeclaredField("alianza2");
+        alianzaReflection.setAccessible(true);
+        Alianza alianza2 = (Alianza)alianzaReflection.get(batalla);
+        assertEquals(20, alianza2.getPersonajes().get(0).getExperiencia());
+        assertEquals(5, alianza2.getPersonajes().get(0).getNivel());
+        assertEquals(20, alianza2.getPersonajes().get(1).getExperiencia());
+        assertEquals(5, alianza2.getPersonajes().get(1).getNivel());
+    }
+    
 
     private void set(String campo, Object valor) throws NoSuchFieldException, IllegalAccessException {
         Field cantidadMuertesAlianza1 = batallaReflection.getDeclaredField(campo);
